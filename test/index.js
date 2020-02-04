b387a26d1212a9e12fdda9b4f7dfe666cefabdd1
Stefan Peters <stefandesu@exo.pm>
2020-02-04 11:58:19 +0900

    Add more tests

--- test/index.js
+++ test/index.js
@@ -124,6 +124,63 @@ describe("annotationsHelper", () => {
 
 })
 
+describe("generateID", () => {
+
+  it("should generate 5000 different random IDs", () => {
+    let ids = []
+    for (let i = 0; i < 5000; i += 1) {
+      let id = utils.generateID()
+      assert.ok(!ids.includes(id), "Generated ID should not exist yet")
+      assert.ok(id.length >= 18 && id.length <= 25, `Length of ID ${id} not in expected range (expected: between 18 and 25, actual: ${id.length})`)
+      ids.push(id)
+    }
+  })
+
+})
+
+describe("hash", () => {
+
+  it("should generate the same hash for different calls with the same string", () => {
+    const hash1 = utils.hash("hello world")
+    const hash2 = utils.hash("hello world")
+    assert.equal(hash1, hash2, `Hash values should be equal: ${hash1} and ${hash2}`)
+  })
+
+  it("should generate two different hashes for two different values", () => {
+    const hash1 = utils.hash("hello world")
+    const hash2 = utils.hash("hello wolrd")
+    assert.notEqual(hash1, hash2, `Hash values should not be equal: ${hash1} and ${hash2}`)
+  })
+
+})
+
+describe("notation", () => {
+
+  it("should return the first notation of an item", () => {
+    let test = {
+      notation: ["1", "2"],
+    }
+    let notation = utils.notation(test)
+    assert.equal(notation, test.notation[0], `Did not return the first notation (expected: ${test.notation[0]}, actual: ${notation})`)
+  })
+
+  it("should uppercase the notation for a concept scheme", () => {
+    let test1 = {
+      notation: ["test"],
+    }
+    let notation1 = utils.notation(test1, "scheme")
+    assert.equal(notation1, test1.notation[0].toUpperCase(), `Notation for scheme should be uppercased: ${notation1}`)
+
+    let test2 = {
+      notation: ["test"],
+      type: ["http://www.w3.org/2004/02/skos/core#ConceptScheme"],
+    }
+    let notation2 = utils.notation(test2)
+    assert.equal(notation2, test2.notation[0].toUpperCase(), `Notation for scheme should be uppercased: ${notation2}`)
+  })
+
+})
+
 describe("prefLabel", () => {
 
   const test = {
@@ -137,23 +194,33 @@ describe("prefLabel", () => {
     assert.equal(utils.prefLabel(test), test.prefLabel.en, "Did not return the expected English label")
   })
 
+  it("should return the German label when providing a preferred language", () => {
+    assert.equal(utils.prefLabel(test, { language: "de" }), test.prefLabel.de, "Did not return the expected German label")
+  })
+
   it("should return the German label after setting option", () => {
     utils.setOption("languages", ["de"])
     assert.equal(utils.prefLabel(test), test.prefLabel.de, "Did not return the expected German label")
+    // Reset option after test
+    utils.setOption("languages", ["en"])
+  })
+
+  it("should correctly handle fallbackToUri option", () => {
+    // Item without prefLabel
+    const test = {
+      uri: "test:hello",
+    }
+    assert.equal(utils.prefLabel(test), test.uri, "Did not fall back to URI as expected")
+    assert.equal(utils.prefLabel(test, { fallbackToUri: false }), "", "Did not respect fallbackToUri option")
   })
 
 })
 
 /**
  * TODO:
- * - generateID
- * - hash
- * - notation
  * - getLanguage
  * - languageMapContent
- * - prefLabel
  * - definition
  * - dateToString
  * - registryStored
- * - reactivity via store or languages array
  */
